29942b7654f5ec8a238c46422eb9de21fbd1060d
Veeno <sebastien.cloiseau@gmail.com>
2024-12-18 17:19:37 +0100

    feature: Simple Wallet SC

--- packages/foundry/contracts/YourContract.sol
+++ packages/foundry/contracts/YourContract.sol
@@ -1,88 +1,77 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+contract MonadTokenWallet is ReentrancyGuard, Ownable {
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-  // State Variables
-  address public immutable owner;
-  string public greeting = "Building Unstoppable Apps!!!";
-  bool public premium = false;
-  uint256 public totalCounter = 0;
-  mapping(address => uint256) public userGreetingCounter;
+  event Deposit(address indexed _user, uint _amount, uint _timestamp);
+  event Withdraw(address indexed _user, uint _amount, uint _timestamp);
+  event AddToken(address indexed user, address indexed tokenAddress);   
 
-  // Events: a way to emit log statements from smart contract that can be listened to by external parties
-  event GreetingChange(
-    address indexed greetingSetter,
-    string newGreeting,
-    bool premium,
-    uint256 value
-  );
+  IERC20 public token;
 
-  // Constructor: Called once on contract deployment
-  // Check packages/foundry/deploy/Deploy.s.sol
-  constructor(
-    address _owner
-  ) {
-    owner = _owner;
-  }
+  mapping(address => mapping(address => bool)) private userTokens;
+  mapping(address => uint) private balances;
+  
+  /**
+    * @dev Constructor => set ERC20 token address
+    * @param _tokenAddress ERC20 token address
+  */
 
-  // Modifier: used to define a set of rules that must be met before or after a function is executed
-  // Check the withdraw() function
-  modifier isOwner() {
-    // msg.sender: predefined variable that represents address of the account that called the current function
-    require(msg.sender == owner, "Not the Owner");
-    _;
+  constructor(address _tokenAddress) Ownable(msg.sender) {
+      if(_tokenAddress != address(0)) revert("Invalid token address");
+      token = IERC20(_tokenAddress);
   }
 
   /**
-   * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-   *
-   * @param _newGreeting (string memory) - new greeting to save on the contract
-   */
-  function setGreeting(
-    string memory _newGreeting
-  ) public payable {
-    // Print data to the anvil chain console. Remove when deploying to a live network.
+    * @dev Deposit tokens to wallet
+    * @param _amount Deposit amount
+  */
 
-    console.logString("Setting new greeting");
-    console.logString(_newGreeting);
+  function deposit(uint _amount) external nonReentrant {
+    if(_amount == 0) revert("Amount must be greater than 0");
+    if(!token.transferFrom(msg.sender, address(this), _amount)) revert("Transfer failed");
 
-    greeting = _newGreeting;
-    totalCounter += 1;
-    userGreetingCounter[msg.sender] += 1;
-
-    // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-    if (msg.value > 0) {
-      premium = true;
-    } else {
-      premium = false;
-    }
-
-    // emit: keyword used to trigger an event
-    emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+    balances[msg.sender] += _amount;
+    emit Deposit(msg.sender,_amount, block.timestamp);
   }
 
   /**
-   * Function that allows the owner to withdraw all the Ether in the contract
-   * The function can only be called by the owner of the contract as defined by the isOwner modifier
-   */
-  function withdraw() public isOwner {
-    (bool success,) = owner.call{ value: address(this).balance }("");
-    require(success, "Failed to send Ether");
+    * @dev Withdraw tokens from wallet
+    * @param _amount Amount withdrawn
+  */
+
+  function withdraw(uint _amount) external nonReentrant {
+    if(_amount == 0) revert("Amount must be greater than 0");
+    if(balances[msg.sender] < _amount) revert("Insuficient balance");
+
+    balances[msg.sender] -= _amount;
+    if (!token.transfer(msg.sender, amount)) revert("Transfer failed");
+    emit Withdraw(msg.sender, _amount, block.timestamp);
   }
 
   /**
-   * Function that allows the contract to receive ETH
-   */
-  receive() external payable { }
+    * @dev Get Token balance
+    * @return User token balance
+  */
+
+  function getBalance() external view returns(uint) {
+    return balances[msg.sender];
+  }
+
+  /**
+    * @dev Add new token to user wallet
+    * @param _tokenAddress Address of token to add
+    * @notice Allows users to add new token to their own wallet
+  */
+
+  function addToken(address _tokenAddress) external {
+    if(_tokenAddress == address(0)) revert("Invalid token address");
+    userTokens[msg.sender][_tokenAddress] = true;
+    emit AddToken(msg.sender, _tokenAddress);
+  }
+  
 }
